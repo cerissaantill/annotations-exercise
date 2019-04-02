92e9f701e8c9c66b29b7a48204f0e6a5d43fb133
Cerissa Antill <cerissa.antill@gmail.com>
2019-04-02 14:06:27 -0500

    fixed the error and added comments to Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        Author author = new Author("Sandi", "Metz");
+        Author author = new Author("Sandi", "Metz");    // creating author object
 
-        author.addBook("Practical Object-Oriented Design in Ruby");
+        author.addBook("Practical Object-Oriented Design in Ruby");     // adding books by that author
         author.addBook("99 Bottles of OOP");
 
-        for (String book: author.getBooks()) {
+        for (String book: author.getBooks()) {      // looping through list of books on Author object
             System.out.println(book);
         }
     }
